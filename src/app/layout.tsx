9f3b80b3aa42a5176949fe2e156491c48f8e25c0
Alexander Chen <alexander.qili.chen@gmail.com>
2024-03-10 09:23:35 +0800

    Added social images, improved title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import { Inter } from "next/font/google";
+import SocialImg from "@/assets/social.png";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Keyboard Layouts",
+  title: "Keyboard Layout Tester",
   description:
-    "Seamlessly try QWERTY, Dvorak, and Colemak keyboard layouts on dummy text.",
+    "Try QWERTY, Dvorak, and Colemak alt keyboard layouts. Seamlessly switch between the layouts and practice typing on dummy text.",
 };
 
 export default function RootLayout({
@@ -18,6 +19,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://keyboard.experimental.software/"
+      />
+      <meta
+        property="og:title"
+        content="Practice Typing with Alt Keyboard Layouts"
+      />
+      <meta
+        property="og:description"
+        content="Try QWERTY, Dvorak, and Colemak alt keyboard layouts. Seamlessly switch between the layouts and practice typing on dummy text."
+      />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:url"
+        content="https://keyboard.experimental.software/"
+      />
+      <meta
+        name="twitter:title"
+        content="Practice Typing with Alt Keyboard Layouts"
+      />
+      <meta
+        name="twitter:description"
+        content="Try QWERTY, Dvorak, and Colemak alt keyboard layouts. Seamlessly switch between the layouts and practice typing on dummy text."
+      />
+
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-3XWS80C1HX"
         strategy="afterInteractive"
